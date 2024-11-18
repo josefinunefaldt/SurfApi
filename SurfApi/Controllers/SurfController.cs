2f3d5f9650db0023f0628f8612030999de52e000
josefinunefaldt <josefin6@hotmail.com>
2024-11-18 12:38:45 +0100

    Rename variables

--- SurfApi/Controllers/SurfController.cs
+++ SurfApi/Controllers/SurfController.cs
@@ -6,31 +6,31 @@ namespace SurfApi.Controllers;
 [Route("[controller]")]
 public class SurfController : ControllerBase
 {
-    readonly HttpClient httpClient = new();
-    readonly HttpClient GeohttpClient = new();
+    readonly HttpClient SurfHttpClient = new();
+    readonly HttpClient GeoHttpClient = new();
 
     private readonly string ApiKey = "ee7c013a-95fb-11ee-8b92-0242ac130002-ee7c01b2-95fb-11ee-8b92-0242ac130002";
 
     private readonly string ApiKeyGeo = "93f84c5d5bd64db5a41ee93c00efefa3";
-    private readonly string url = $"https://api.geoapify.com/v1/geocode/search?text={placeName}&apiKey={ApiKeyGeo}";
+
 
 
     [HttpGet]
     public async Task<SurfResponse> Get(string placeName)
     {
+        var url = $"https://api.geoapify.com/v1/geocode/search?text={placeName}&apiKey={ApiKeyGeo}";
 
-
-        GeohttpClient.DefaultRequestHeaders.Add("Authorization", ApiKey);
-        var GeoCodeResponse = await httpClient.GetFromJsonAsync<GeoCodeResponse>
+        GeoHttpClient.DefaultRequestHeaders.Add("Authorization", ApiKey);
+        var GeoCodeResponse = await GeoHttpClient.GetFromJsonAsync<GeoCodeResponse>
         (url);
 
-        var longi = GeoCodeResponse.features[0].properties.lon.ToString();
-        var lati = GeoCodeResponse.features[0].properties.lat.ToString();
+        var longitude = GeoCodeResponse.features[0].properties.lon.ToString();
+        var latiude = GeoCodeResponse.features[0].properties.lat.ToString();
 
 
-        httpClient.DefaultRequestHeaders.Add("Authorization", ApiKey);
-        var surfResponse = await httpClient.GetFromJsonAsync<SurfResponse>
-        ($"https://api.stormglass.io/v2/weather/point?lat={lati}&lng={longi}&params=waveHeight");
+        SurfHttpClient.DefaultRequestHeaders.Add("Authorization", ApiKey);
+        var surfResponse = await SurfHttpClient.GetFromJsonAsync<SurfResponse>
+        ($"https://api.stormglass.io/v2/weather/point?lat={latiude}&lng={longitude}&params=waveHeight");
 
         return surfResponse;
     }
